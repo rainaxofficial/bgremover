b4a0a9a21d6c84a27f63b58037a87eb3a90f2637
rainaxofficial <rainaxofficial@gmail.com>
2025-08-04 01:11:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,5 @@ def remove_background():
         download_name="output.png"
     )
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(port=5000)
